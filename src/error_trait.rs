5b9e812184c797b64235fe96239117d278b0f2bc
roblabla <unfiltered@roblab.la>
2019-10-08 21:26:40 +0000

    Tentatively remove description fn
    The description function is deprecated and shouldn't be used in new
    code. If someone makes a compelling case to add it back, we'll cross
    that bridge then.

--- src/error_trait.rs
+++ src/error_trait.rs
@@ -22,10 +22,6 @@ use alloc::boxed::Box;
 /// [`Debug`]: ../fmt/trait.Debug.html
 /// [`source`]: trait.Error.html#method.source
 pub trait Error: Debug + Display + TypeInfo {
-    fn description(&self) -> &str {
-        "description() is deprecated; use Display"
-    }
-
     fn cause(&self) -> Option<&Error> {
         self.source()
     }
