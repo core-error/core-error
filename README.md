c5a96e5ee3fda23ba073192f2d30474f115823e9
roblabla <unfiltered@roblab.la>
2019-10-27 13:27:46 +0000

    Some documentation fixes

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Core Error
 
+Traits for working with `Error` in `std`-less environments.
+
+## Warning
+
+This is a pre-release meant to allow experimentation and integration into the
+various error handling crates. Please do not use it yet! 1.0.0 is right around
+the corner.
+
+## Rationale
+
 With the recent proliferation of error-handling crates, it has become clear that
 the situation around the lack of a `core::error::Error` is really suboptimal. In
 `snafu`, `no_std` support is being introduced through a whole new Error trait
@@ -8,8 +18,8 @@ becoming incompatible with the ecosystem.
 
 Ideally, the `Error` trait would show up in core, but due to coherence concerns
 and std-dependent features being added to `std::error::Error`, a resolution is
-unlikely to happen soon.  As such, I propose making a new crate, `core-error` -
-exposing our own version of the `Error `trait. The goal of this crate is
+unlikely to happen soon. As such, I propose making a new crate, `core-error` -
+exposing our own version of the `Error` trait. The goal of this crate is
 twofolds:
 
 - Provide a common trait for various error handling crates (Failure, Snafu,
@@ -22,13 +32,15 @@ for integration in the various error crates. Furthermore, once it reaches 1.0.0,
 it will follow the same stability guarantees Rust does.
 
 # Usage
+
 This crate simply provides an Error trait, which is identical to the one in std
 except for not providing deprecated methods. It also contains two features:
 
 - `std`: simply reexport `std::error::Error`
 - `alloc`: implement Error on alloc Errors (incl. Box)
 
-Libraries using this crate should forward their equivalent features to this library.
+Libraries using this crate should forward their equivalent features to this
+library.
 
 # Minimum Rust Version
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,37 @@
 //! Traits for working with `Error` in `std`-less environments.
 //!
+//! # Warning
+//!
+//! This is a pre-release meant to allow experimentation and integration into
+//! the various error handling crates. Please do not use it yet! 1.0.0 is right
+//! around the corner.
+//!
 //! # Usage
 //!
-//! This crate provides an [`Error`](trait.Error.html) trait which is identical to
-//! [the one in std][std-error], except for not providing deprecated methods.
+//! This crate provides an [`Error`](trait.Error.html) trait which is identical
+//! to [the one in std][std-error], except for not providing deprecated methods.
 //!
 //! This library also exposes two opt-in features:
 //!
 //! - `std`: simply re-export [`std::error::Error`][std-error].
-//! - `alloc`: implement the [`Error`](trait.Error.html) trait on `alloc` errors (including boxed types).
+//! - `alloc`: implement the [`Error`](trait.Error.html) trait on `alloc` errors
+//!   (including boxed types).
 //!
-//! Libraries using this crate should forward their equivalent features to this library.
+//! Libraries using this crate should forward their equivalent features to this
+//! library.
 //!
 //! # Minimum Supported Rust Version (MSRV)
 //!
-//! This crate works all the way down to 1.0.0. It auto-detects the Rust version in
-//! order to know which error structs to implement the trait on.
+//! This crate works all the way down to 1.0.0. It auto-detects the Rust version
+//! in order to know which error structs to implement the trait on.
 //!
-//! With `no-default-features`, the crate only compiles from 1.6.0 onwards (version
-//! at which `no_std` became stable).
+//! With `no-default-features`, the crate only compiles from 1.6.0 onwards
+//! (version at which `no_std` became stable).
 //!
 //! [std-error]: https://doc.rust-lang.org/std/error/trait.Error.html
 
 // If std feature is disabled, this crate is no_std.
-// This avoids making this crate fail on std build in version 1.5.0 and under
+// On 1.5.0 and under, no_std is unstable, so we can't just unconditionally use it.
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(unknown_lints)]
 #![allow(bare_trait_objects)]
